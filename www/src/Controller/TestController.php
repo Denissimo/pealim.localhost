96feefadf71cbc7b9ee3284e43cc4cab50d49e89
Den <d.drozdetsky@faberlic.com>
2024-10-05 11:33:06 +0300

    message 2 rabbit works

--- www/src/Controller/TestController.php
+++ www/src/Controller/TestController.php
@@ -4,14 +4,21 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Message\TestMessage;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class TestController extends AbstractController
 {
+    /**
+     * @throws ExceptionInterface
+     */
     #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    public function index(MessageBusInterface $bus): Response
     {
         $a = 12;
+        $bus->dispatch(new TestMessage('testt'));
 
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
